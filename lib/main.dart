eb8a479accea62bd7f5757931bd49c37b3923bca
oxelo99 <creangarazvan99@gmail.com>
2023-05-17 13:39:03 +0300

    very first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Auth());
+}
+
+class Auth extends StatelessWidget {
+  const Auth({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
